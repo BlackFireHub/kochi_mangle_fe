3cd0c3da1d77313c93d48e7e2481b16208caf9af
seoyoonyi <yiseoyoon@gmail.com>
2025-02-20 11:11:12 +0900

    chore: GDS 라이브러리 예제 적용 (#4)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,26 @@
+import { useRef } from 'react'
+
+import { Button, TextInput } from '@goorm-dev/vapor-core'
+import { SearchOutlineIcon } from '@goorm-dev/vapor-icons'
+
 import { useCounter } from '../store/useCounter'
 
 const Home = () => {
   const { count, increase } = useCounter()
+  const ref = useRef<HTMLDivElement>(null)
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-red">Count: {count}</p>
-      <button onClick={increase}>Increase</button>
+      <div ref={ref} role="dialog">
+        <p className="text-red">Count: {count}</p>
+
+        <Button color="primary" onClick={increase}>
+          Increase
+          <SearchOutlineIcon />
+        </Button>
+        <TextInput type="text" placeholder="Enter your name" />
+      </div>
     </div>
   )
 }
