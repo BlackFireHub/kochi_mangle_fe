8dd0aed80ddc46a028d2aa7f0f96440a1caed773
seoyoonyi <yiseoyoon@gmail.com>
2025-02-20 17:54:00 +0900

    feat: 온보딩, 스플래쉬 페이지 삭제 및 스플래쉬 컴포넌트 추가 (#13)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,5 @@
 export const ROUTES = {
   HOME: '/',
-  ONBOARDING: '/onboarding',
-  CATEGORIES: '/categories',
   CLASS_LIST: (slug: string) => `/class-list/${slug}`,
   CLASS_DETAIL: (slug: string, id: string) => `/class-list/${slug}/${id}`,
 } as const
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react'
+
 import { Outlet } from 'react-router-dom'
 
+import SplashScreen from '@/components/SplashScreen'
+
 const RootLayout = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="relative min-h-screen bg-gray-100">
       <main className="absolute top-0 right-0 bottom-0 left-0 m-auto w-full max-w-[375px] bg-white shadow-md">
-        <Outlet />
+        {isLoading ? <SplashScreen /> : <Outlet />}
       </main>
     </div>
   )
