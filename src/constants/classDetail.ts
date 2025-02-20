d37e9aad88ca85a83808ef1a0985a6af0f522c9f
seoyoonyi <yiseoyoon@gmail.com>
2025-02-21 02:57:10 +0900

    feat: 클래스 상세페이지 목데이터 추가

---
+++ src/constants/classDetail.ts
@@ -0,0 +1,20 @@
+import classList1 from '@/assets/class/class_list_1.jpg'
+import user1 from '@/assets/user/1.jpeg'
+
+export const classDetail = [
+  {
+    id: '1',
+    img: classList1,
+    name: '해녀의 바당요리1',
+    description: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요.',
+    endDate: '2025-07-12',
+    location: '제주시 이도 2동',
+    foodIntro:
+      '제주 인심이 담겨있는 별미 성게국과 임금님께 진상되던 옥돔을 직접 굽고 맛보면, 제주 바다는 절대 잊을 수 없을 거예요.',
+    participants: 9,
+    price: 15000,
+    userImg: user1,
+    userNickname: '해녀 김바당',
+    userInfo: '20년 경력 바다의 이야기꾼',
+  },
+]
