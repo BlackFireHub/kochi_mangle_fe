ceedcb138a1d4fd0c5a21199adf708afacaef5c6
junghwan.shin <shjohw11@naver.com>
2025-02-21 04:46:08 +0900

    .

--- src/pages/ClassDetailPage.tsx
+++ src/pages/ClassDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { Button, IconButton, Text } from '@goorm-dev/vapor-core'
 import {
@@ -8,15 +8,33 @@ import {
   PriceOutlineIcon,
   UserIcon,
 } from '@goorm-dev/vapor-icons'
+import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { classDetail } from '@/constants/classDetail'
+import { imgList } from '@/constants/classList'
+
+interface Clazz {
+  category_id: number
+  class_id: number
+  due_date: string
+  loc: string
+  now_participants: number
+  max_participants: number
+  how_much: number
+  teacher_name: string
+  teacher_article: string
+  class_name: string
+  class_article: string
+  class_detail: string
+  is_closed: boolean
+}
 
 const ClassDetailPage = () => {
   const navigate = useNavigate()
   const maxParticipants = 10
   const currentParticipants = classDetail[0].participants
-  const { slug } = useParams<{ slug: string; id: string }>()
+  const { slug, id } = useParams<{ slug: string; id: string }>()
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -25,6 +43,23 @@ const ClassDetailPage = () => {
   const handleNextClick = () => {
     alert('배우러 가기 버튼이 클릭되었습니다!')
   }
+
+  const [clazz, setClazz] = useState<Clazz | null>(null)
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axios.get(
+          `https://k92dd0c11630da.user-app.krampoline.com/api/class/${id}`,
+        )
+        setClazz(response.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetch()
+  }, [])
 
   return (
     <div className="p-[var(--space-200)]">
@@ -47,112 +82,114 @@ const ClassDetailPage = () => {
         </IconButton>
       </div>
 
-      <div>
-        <div className="mb-[var(--space-200)]">
-          <div className="mb-[var(--space-200)] flex h-[193px] overflow-hidden rounded-[var(--space-200)] border border-[var(--border-color)] bg-[var(--gray-100)]">
-            <img
-              src={classDetail[0].img}
-              alt={classDetail[0].name}
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <Text as="h2" typography="heading4">
-            {classDetail[0].name}
-          </Text>
-          <Text as="p" typography="subtitle1" color="foreground-hint">
-            {classDetail[0].description}
-          </Text>
-        </div>
-        <div className="mb-[var(--space-200)] rounded-[var(--border-radius-500)] bg-[var(--gray-100)] p-[var(--space-200)]">
-          <div className="mb-[var(--space-200)] flex gap-[var(--space-200)]">
-            <div className="flex h-[48px] w-[48px] overflow-hidden rounded-4xl bg-[var(--gray-100)]">
+      {clazz && (
+        <div>
+          <div className="mb-[var(--space-200)]">
+            <div className="mb-[var(--space-200)] flex h-[193px] overflow-hidden rounded-[var(--space-200)] border border-[var(--border-color)] bg-[var(--gray-100)]">
               <img
-                src={classDetail[0].userImg}
-                alt={classDetail[0].userNickname}
+                src={imgList[clazz.class_id - 1]}
+                alt={clazz.class_name}
                 className="h-full w-full object-cover"
               />
             </div>
-            <div>
-              <Text as="h2" typography="heading5">
-                {classDetail[0].userNickname}
-              </Text>
-              <Text as="p" typography="subtitle1" color="foreground-hint">
-                {classDetail[0].userInfo}
-              </Text>
+            <Text as="h2" typography="heading4">
+              {clazz.class_name}
+            </Text>
+            <Text as="p" typography="subtitle1" color="foreground-hint">
+              {clazz.class_article}
+            </Text>
+          </div>
+          <div className="mb-[var(--space-200)] rounded-[var(--border-radius-500)] bg-[var(--gray-100)] p-[var(--space-200)]">
+            <div className="mb-[var(--space-200)] flex gap-[var(--space-200)]">
+              <div className="flex h-[48px] w-[48px] overflow-hidden rounded-4xl bg-[var(--gray-100)]">
+                <img
+                  src={classDetail[0].userImg}
+                  alt={clazz.teacher_name}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div>
+                <Text as="h2" typography="heading5">
+                  {clazz.teacher_name}
+                </Text>
+                <Text as="p" typography="subtitle1" color="foreground-hint">
+                  {clazz.teacher_article}
+                </Text>
+              </div>
+            </div>
+
+            <hr className="my-[var(--space-200)] border-[var(--border-color)]" />
+
+            <div className="flex">
+              <div className="mr-[var(--space-200)] flex flex-col gap-[var(--space-100)]">
+                <div className="flex items-center">
+                  <CalendarIcon size={16} className="color-[var(--gray-700)]" />
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    진행기간
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <LocationIcon size={16} className="color-[var(--gray-700)]" />
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    지역
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <UserIcon size={16} className="color-[var(--gray-700)]" />
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    참여 현황
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <PriceOutlineIcon size={16} className="color-[var(--gray-700)]" />
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    가격
+                  </Text>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-[var(--space-100)]">
+                <div className="flex items-center">
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    {clazz.due_date}
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    {clazz.due_date}
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    {clazz.now_participants}
+                  </Text>
+                </div>
+                <div className="flex items-center">
+                  <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
+                    {clazz.how_much}
+                  </Text>
+                </div>
+              </div>
             </div>
           </div>
 
-          <hr className="my-[var(--space-200)] border-[var(--border-color)]" />
+          <Text as="p" typography="subtitle1" color="foreground-hint">
+            {clazz.class_name}
+          </Text>
 
-          <div className="flex">
-            <div className="mr-[var(--space-200)] flex flex-col gap-[var(--space-100)]">
-              <div className="flex items-center">
-                <CalendarIcon size={16} className="color-[var(--gray-700)]" />
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  진행기간
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <LocationIcon size={16} className="color-[var(--gray-700)]" />
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  지역
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <UserIcon size={16} className="color-[var(--gray-700)]" />
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  참여 현황
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <PriceOutlineIcon size={16} className="color-[var(--gray-700)]" />
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  가격
-                </Text>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-[var(--space-100)]">
-              <div className="flex items-center">
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  {classDetail[0].endDate}
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  {classDetail[0].location}
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  {classDetail[0].participants}
-                </Text>
-              </div>
-              <div className="flex items-center">
-                <Text as="p" typography="subtitle1" color="foreground-secondary" className="ml-1">
-                  {classDetail[0].price}
-                </Text>
-              </div>
-            </div>
+          <div className="mt-[16px]">
+            <Button
+              size="xl"
+              color="danger"
+              stretch
+              disabled={currentParticipants >= maxParticipants}
+              onClick={currentParticipants < maxParticipants ? handleNextClick : undefined}
+            >
+              {currentParticipants >= maxParticipants ? '참여 마감' : '배우러 가기'}
+            </Button>
           </div>
         </div>
-
-        <Text as="p" typography="subtitle1" color="foreground-hint">
-          {classDetail[0].foodIntro}
-        </Text>
-
-        <div className="mt-[16px]">
-          <Button
-            size="xl"
-            color="danger"
-            stretch
-            disabled={currentParticipants >= maxParticipants}
-            onClick={currentParticipants < maxParticipants ? handleNextClick : undefined}
-          >
-            {currentParticipants >= maxParticipants ? '참여 마감' : '배우러 가기'}
-          </Button>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
--- src/pages/ClassListPage.tsx
+++ src/pages/ClassListPage.tsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
+import { IconButton, Text } from '@goorm-dev/vapor-core'
+import { CalendarIcon, ChevronLeftOutlineIcon, LocationIcon } from '@goorm-dev/vapor-icons'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-
-import { IconButton, Text } from '@goorm-dev/vapor-core';
-import { CalendarIcon, ChevronLeftOutlineIcon, LocationIcon } from '@goorm-dev/vapor-icons';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-
-
-
-import { CATEGORY_LIST } from '@/constants/categories';
-import { imgList } from '@/constants/classList';
-import { ROUTES } from '@/constants/routes';
-
-
-
-
+import { CATEGORY_LIST } from '@/constants/categories'
+import { imgList } from '@/constants/classList'
+import { ROUTES } from '@/constants/routes'
 
 interface Clazz {
   category_id: number
@@ -45,108 +37,10 @@ const ClassListPage = () => {
   useEffect(() => {
     const fetch = async () => {
       try {
-        const response = await axios.get(`/api/category/1`)
+        const response = await axios.get(
+          `https://k92dd0c11630da.user-app.krampoline.com/api/category/1`,
+        )
         setClassList(response.data)
-
-        // 로컬 테스트용
-        // setClassList([
-        //   {
-        //     category_id: 1,
-        //     class_id: 1,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        //   {
-        //     category_id: 1,
-        //     class_id: 2,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        //   {
-        //     category_id: 1,
-        //     class_id: 3,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        //   {
-        //     category_id: 1,
-        //     class_id: 4,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        //   {
-        //     category_id: 1,
-        //     class_id: 5,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        //   {
-        //     category_id: 1,
-        //     class_id: 6,
-        //     due_date: '2025-07-12',
-        //     loc: '제주시 이도2동',
-        //     now_participants: 2,
-        //     max_participants: 10,
-        //     how_much: 15000,
-        //     teacher_name: '해녀 김바당',
-        //     teacher_article: '20년 경력 바다의 이야기꾼',
-        //     class_name: '해녀의 바당요리',
-        //     class_article: '해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요',
-        //     class_detail:
-        //       '제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.',
-        //     is_closed: false,
-        //   },
-        // ])
       } catch (err) {
         console.error(err)
       }
